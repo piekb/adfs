8edee330d9028d1048680ccb4ffcc38af403784b
marieke <jdsfkl>
2020-07-21 15:14:47 +0200

    for vm 3

--- src/main.py
+++ src/main.py
@@ -76,8 +76,9 @@ def main(argv):
     cur_path = os.getcwd()
     part = os.path.split(cur_path)[0]
 
-    user_in = input("Please enter file name: ")
-    # print(sys.argv[1])
+    # user_in = input("Please enter file name: ")
+    print(sys.argv[1])
+    user_in = sys.argv[1]
     # user_in = 'adfex7'
     path = part + '/ex/' + user_in
     # print(path)
